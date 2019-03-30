57308d8b2ce51a136d77c003ea7b6977c78d4827
Andrayayay <andrayayay@gmail.com>
2019-03-30 18:04:20 -0500

    files created

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name,devoured)
+VALUES  ("PTerry's Black Bean Burger", 0),
+        ("Arlo's Vegan B'con Cheese Burger", 0),
+        ("Boca Burger", 0),
+        ("Kerby Lane's Beet Burger", 0),
+        ("Plow Burger", 0);
